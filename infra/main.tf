10124ad559bd22f9fe5e20438da3e9117d0399fd
sugiyama404 <mmingriha@gmail.com>
2025-05-03 22:38:50 +0900

    Add azure-functions to requirements and remove versioning from docker-compose

--- infra/main.tf
+++ infra/main.tf
@@ -11,7 +11,11 @@ terraform {
 provider "azurerm" {
   resource_provider_registrations = "none"
   subscription_id                 = var.subscription_id
-  features {}
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
 }
 
 resource "azurerm_resource_group" "resource_group" {
