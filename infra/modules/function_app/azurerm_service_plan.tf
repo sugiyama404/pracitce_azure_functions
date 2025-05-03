b71301d1105597e96d71c12413085a66e58dc8a8
sugiyama404 <mmingriha@gmail.com>
2025-05-03 10:42:41 +0900

    Refactor service plan configuration to use variable references for resource group name and location

--- infra/modules/function_app/azurerm_service_plan.tf
+++ infra/modules/function_app/azurerm_service_plan.tf
@@ -1,7 +1,7 @@
 resource "azurerm_service_plan" "example" {
   name                = "notification-func-service-plan"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+  location            = var.resource_group.location
+  resource_group_name = var.resource_group.name
   os_type             = "Linux"
   sku_name            = "Y1" # 消費プラン
 }
